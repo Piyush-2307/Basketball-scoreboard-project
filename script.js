b3ea2d3f7a6a30b4d18900e9e54ddb77f395f24d
Piyush-2307 <peeyusharora831@gmail.com>
2024-08-25 20:47:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+function one(){
+        count += 1
+        countEl.textContent = count
+    }
+    function two(){
+            count += 2
+            countEl.textContent = count
+        }
+        function three(){
+                count += 3
+    countEl.textContent = count
+
+}
+let countE = document.getElementById("countE2")
+let countstr = 0
+function onee(){
+    countstr += 1
+    countE.textContent = countstr
+}
+function twoo(){
+    countstr += 2
+    countE.textContent = countstr
+}
+function threee(){
+    countstr += 3
+    countE.textContent = countstr
+}
